fe52c4e17733c30dd96d888669bdb9fccbbf5d2a
pppkizbroutle <erikrangel.014@ciencias.unam.mx>
2023-05-21 23:25:57 -0600

    Añadiendo direccion a tabla Usuario

--- SQL/DDL.sql
+++ SQL/DDL.sql
@@ -1,7 +1,8 @@
-CREATE TABLE usuario (
+CREATE TABLE public.usuario (
 	id_usuario int4 NOT NULL,
 	nombre bpchar(64) NOT NULL,
 	contrasenia bpchar(128) NOT NULL,
+	direccion varchar NULL,
 	CONSTRAINT usuario_pk PRIMARY KEY (id_usuario),
 	CONSTRAINT usuario_un UNIQUE (nombre)
 );
